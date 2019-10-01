7e8bf7377b33c6dcd9e2d14b4136c9fba00473e0
bbvch13531 <bbvch13531@gmail.com>
2019-10-01 18:43:41 +0900

    Initial commit
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ ListVideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListVideoPlayer
+//
+//  Created by KyungYoung Heo on 01/10/2019.
+//  Copyright © 2019 KyungYoung Heo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
